a9349c3605a55b8580ed56f6bb26d9b52a1a8260
George <georgehargreaves5@outlook.com>
2023-09-24 18:22:29 +0100

    Initial Commit

---
+++ SearchableView/SearchableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableViewApp.swift
+//  SearchableView
+//
+//  Created by George Hargreaves on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
