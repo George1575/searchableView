6071ea1dbfe53b6160f0c31c15cf9bc15be7d9d7
George <georgehargreaves5@outlook.com>
2023-09-24 18:40:36 +0100

    beginnning

--- SearchableView/ContentView.swift
+++ SearchableView/ContentView.swift
@@ -6,18 +6,64 @@
 //
 
 import SwiftUI
+import Foundation
 
-struct ContentView: View {
+struct Subject: Identifiable, Decodable {
+    let id = UUID()
+    let name: String
+    let details: String
+}
+struct SubjectDetailView: View {
+    let subject: Subject
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(subject.details)
+            .padding()
+            .navigationTitle(subject.name)
     }
 }
+
+struct ContentView: View {
+    @State private var selectedSubject: Subject?
+    @State private var searchText = ""
+    @State private var subjects: [Subject] = []
+    
+    var filteredSubjects: [Subject] {
+        if searchText.isEmpty {
+            return subjects
+        } else {
+            return subjects.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List(filteredSubjects) { subject in
+                NavigationLink(destination: SubjectDetailView(subject: subject)) {
+                    Text(subject.name)
+                }
+            }
+            .searchable(text: $searchText)
+            .navigationBarTitle("Subjects")
+            .onAppear(perform: loadSubjects)
+        }
+    }
+    
+    func loadSubjects() {
+        if let url = Bundle.main.url(forResource: "subjects", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                subjects = try JSONDecoder().decode([Subject].self, from: data)
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        } else {
+            print("Cannot find subjects.json")
+        }
+    }
+}
+
+
 
 #Preview {
     ContentView()
